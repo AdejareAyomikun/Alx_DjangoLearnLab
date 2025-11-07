b7cd41ba3b22ff83cbf9225544c23f2cb2febf90
Adejare Faruk <farukpamilerin@gmail.com>
2025-11-07 09:20:01 +0100

    Added urls.py, templates/library_details.html, templates/list_books.html and updated views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+def book_list_fbv(request):
+    """Lists all books using a function-based view."""
+    # Retrieve all Book objects
+    all_books = Book.objects.all().select_related('author')
+    
+    context = {
+        'books': all_books,
+        'view_type': 'Function-Based View'
+    }
+    return render(request, 'list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    """Displays details for a specific library using a DetailView CBV."""
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library' 
+
+    def get_context_data(self, **kwargs):
+        """Adds custom context data to the template."""
+        context = super().get_context_data(**kwargs)
+        context['view_type'] = 'Class-Based View'
+        return context
