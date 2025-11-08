09326e797cf031acc12f065779aee681428c0310
Adejare Faruk <farukpamilerin@gmail.com>
2025-11-08 11:02:49 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect , get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
@@ -6,35 +6,40 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
+
 def list_books(request):
     """Lists all books using a function-based view."""
     # Retrieve all Book objects
     all_books = Book.objects.all().select_related('author')
-    
+
     context = {
         'books': all_books,
         'view_type': 'Function-Based View'
     }
     return render(request, 'relationship_app/list_books.html', context)
 
+
 class LibraryDetailView(DetailView):
     """Displays details for a specific library using a DetailView CBV."""
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library' 
+    context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         """Adds custom context data to the template."""
         context = super().get_context_data(**kwargs)
         context['view_type'] = 'Class-Based View'
         return context
-    
+
+
 class LoginView(login):
     template_name = 'registration/login.html'
 
+
 class LogoutView(logout):
     template_name = 'registration/logged_out.html'
-    
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -42,6 +47,6 @@ def register(request):
             form.save()
             return redirect('login')
         else:
+            form = UserCreationForm()
             context = {'form': form}
-            return render(request, 'registration/register.html', context)
-        
+            return render(request, 'relationship_app/register.html', context)
