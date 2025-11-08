c66a40ca5fa3a2fab2e3fba68692bdceedb1b996
Adejare Faruk <farukpamilerin@gmail.com>
2025-11-08 10:57:12 +0100

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-from .views import LoginView
 from .views import LogoutView
+from .views import LoginView
 from .views import LibraryDetailView
 from .views import list_books
 
