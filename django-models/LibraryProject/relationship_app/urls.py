84425af93adae0c38a6ffa732d310b7cc13cbfb9
Adejare Faruk <farukpamilerin@gmail.com>
2025-11-09 23:55:04 +0100

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('admin_area/', views.admin_view, name='admin_area'),
     path('librarian_area/', views.librarian_view, name='librarian_area'),
     path('member_area/', views.member_view, name='member_area'),
-    path('books/add/', views.add_book, name='book_add'),
-    path('books/edit/<int:pk>/', views.edit_book, name='book_edit'),
+    path('books/add/', views.add_book, name='book_add/'),
+    path('books/edit/<int:pk>/', views.edit_book, name='book_edit/'),
     path('books/delete/<int:pk>/', views.delete_book, name='book_delete'),
 ]
