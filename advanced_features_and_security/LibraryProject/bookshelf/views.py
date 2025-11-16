3b118beae196fc07abb211ccbec520ccd46538f7
Adejare Faruk <farukpamilerin@gmail.com>
2025-11-16 16:09:40 +0100

    update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the Book Shelf!")
+
+def book_list(request):
+    return render(request, 'raise_exception', "books")
